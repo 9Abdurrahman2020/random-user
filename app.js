f81768cf88f5fcf5303e1d3236e800b6a91f62d7
Abdur Rahman <9abdurrahman2020@gmail.com>
2022-09-15 16:58:31 +0600

    reorganized root route code

--- app.js
+++ app.js
@@ -4,16 +4,15 @@ const router = require("./routes/user.routes");
 const app = express();
 app.use(express.json());
 app.use("/user", router);
-
-// not found route handler
-app.all("*", (req, res) => {
-  res.status(404).send("Route not found");
-});
-
+// root route
 app.get("/", (req, res) => {
   res.send({
     message: "Server is live",
   });
+});
+// not found route handler
+app.all("*", (req, res) => {
+  res.status(404).send("Route not found");
 });
 
 app.listen(5000, () => console.log("Server is running"));
